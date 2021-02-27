7e83ca22be8b432b4e19ebd5d9ac0364921d65ea
John Kenedy <john.kenedy0410@gmail.com>
2021-02-26 21:20:11 -0300

    Done: Pass information to fragment

---
+++ app/src/main/java/com/canytech/jetpacknavigation/EnterDetailsFragment.kt
@@ -0,0 +1,59 @@
+package com.canytech.jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class EnterDetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
+
+        val etName = rootView.findViewById<EditText>(R.id.et_name)
+        val etMobileNumber = rootView.findViewById<EditText>(R.id.et_mobile)
+
+
+        val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
+        btnVerifyDetails.setOnClickListener {
+
+            val firstName = etName.text.toString()
+            val mobile = etMobileNumber.text.toString()
+
+            when {
+                firstName.isEmpty() -> {
+                    Toast.makeText(activity, "Enter Name.", Toast.LENGTH_SHORT).show()
+                }
+                mobile.isEmpty() -> {
+                    Toast.makeText(activity, "Enter Mobile", Toast.LENGTH_SHORT).show()
+                }
+                //Moving data
+                else -> {
+                    val bundle = bundleOf(
+                        "name" to firstName,
+                        "mobile" to mobile.toLong()
+                    )
+                    //Set Bundle
+                    findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment
+                        ,bundle)
+                }
+            }
+        }
+        return rootView
+    }
+}
---
+++ app/src/main/java/com/canytech/jetpacknavigation/VerifyDetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.canytech.jetpacknavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class VerifyDetailsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        val name = arguments?.getString("name")
+        val mobileNumber = arguments?.getLong("mobile")
+
+        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
+        var tvName = rootView.findViewById<TextView>(R.id.tv_name)
+        var tvMobileNumber = rootView.findViewById<TextView>(R.id.tv_mobile_number)
+
+        tvName.text = name
+        tvMobileNumber.text = mobileNumber.toString()
+
+        // Inflate the layout for this fragment
+        return rootView
+    }
+}
